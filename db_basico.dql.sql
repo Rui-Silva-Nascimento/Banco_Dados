6ac91c41e9d98281ee4c5e8e4c847c7be0c49a7b
Rui-Silva-Nascimento <57668890+Rui-Silva-Nascimento@users.noreply.github.com>
2020-07-20 13:58:09 -0300

    Comandos básicos SQL
    Criação, Visualização, Seleção e Remoção de um DataBase

---
+++ db_basico.dql.sql
@@ -0,0 +1,7 @@
+-- create database if not exists db_Cadastro;  -- Criação do banco de dados.
+-- show databases;   -- #Apresenta os bancos de dados. 
+-- use db_cadastro;  -- #Seleciona o banco de dados 'use name_database'.
+-- select database();   -- #Apresenta qual o banco de dados.
+-- drop database if exists cadastro; -- #Exclusão de um banco de dados existente.
+-- show tables; -- #Mostra tabelas criadas dentro do banco de dados.
+
